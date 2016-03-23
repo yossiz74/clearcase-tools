d53fc5bcb66b76d9102a2fa7df06665951ecb51e
Yossi Zinger <yossiz@algotec.co.il>
2016-03-24 01:30:53 +0200

    implemented argument validation (except patch outside of cc and some helper fnction)

--- cc_checkin_trend/cc_checkin_trend/cc_checkin_trend.py
+++ cc_checkin_trend/cc_checkin_trend/cc_checkin_trend.py
@@ -1,4 +1,7 @@
-import argparse;
+import argparse, subprocess, os
+
+MIN_DAYS = 1
+MAX_DAYS = 7
 
 def create_parser():
     parser = argparse.ArgumentParser(
@@ -17,15 +20,42 @@ def create_parser():
 
     parser.add_argument(
         '-d', '--days', type=int, required=True,
-        help='trend length'
+        help='trend length in days (' + str(MIN_DAYS) + "-" + str(MAX_DAYS) + ')'
     )
 
     return parser
+
+def valid_branch(branch,vob):
+    retcode = subprocess.call(["cleartool", "lstype","-s", "brtype:" + branch + "@\\" + vob])
+    return (retcode==0)
+
+def valid_days(days):
+    return ((days >= MIN_DAYS) and (days <= MAX_DAYS)) 
+
+def valid_path(path):
+    return os.path.isdir(path);
+
+def get_vob_of_path(path):
+    return "Unknown"
     
+def validate_args(args):
+    if not valid_path(args.path):
+        print "Invalid path \'" + args.path+ "\'\n"
+        raise SystemExit
+    vob=get_vob_of_path(args.path)
+    if not valid_branch(args.branch,vob):
+        print "Unknown branch type \'" + args.branch + "\'\n"
+        raise SystemExit
+    if not valid_days(args.days):
+        print "Invalid number of days \'" + args.path+ "\', must be between "  + str(MIN_DAYS) + "-" + str(MAX_DAYS) + "\n"
+        raise SystemExit
+    return;
+
 def main():
     parser = create_parser()
     args = parser.parse_args()
-    print "Computing check-in trend in branch " + args.branch + " for " + str(args.days) + " days under " + args.path + "\n"
+    validate_args(args)
+    print "Computing check-in trend in branch \'" + args.branch + "\' for " + str(args.days) + " days under " + args.path + "\n"
 
 if __name__ == '__main__':
     main()
--- cc_checkin_trend/cc_checkin_trend/test_cc_checkin_trend.py
+++ cc_checkin_trend/cc_checkin_trend/test_cc_checkin_trend.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from cc_checkin_trend import create_parser
+from cc_checkin_trend import create_parser, MAX_DAYS, MIN_DAYS, valid_branch, valid_days, valid_path, get_vob_of_path
 
 # Inspired by http://dustinrcollins.com/testing-python-command-line-apps
 class CommandLineTestCase(TestCase):
@@ -24,5 +24,41 @@ class ArgParserTestCases(CommandLineTestCase):
         with self.assertRaises(SystemExit):
             self.parser.parse_args(['\\scm\\scripts', '\\vob1\\mst\\c#', '-b', 'main', '-d', '7'])
 
+class ArgValidatorTestCases(TestCase):
+    def test_branch_existing_in_vob(self):
+        vob_tag="vob1" # TODO use cleartool lsvob and pick an active one
+        self.assertTrue(valid_branch('main',vob_tag))
+    def test_branch_nonexisting_in_vob(self):
+        vob_tag="vob1" # TODO use cleartool lsvob and pick an active one
+        self.assertFalse(valid_branch('FHAFIFUILWQ',vob_tag))
+    def test_branch_nonexisting_vob(self):
+        vob_tag="uyrfaiofnmal"
+        self.assertFalse(valid_branch('main',vob_tag))
+    def test_existing_cc_path(self):
+        vob_tag="vob1" # TODO use cleartool lsvob and pick an active one
+        view_root="V:" # TODO use cleartool lsview and pick one
+        self.assertTrue(valid_path(view_root + "\\" + vob_tag)) 
+    def test_missing_path(self):
+        vob_tag="vob1" # TODO use cleartool lsvob and pick an active one
+        view_root="V:" # TODO use cleartool lsview and pick one
+        self.assertFalse(valid_path(view_root + "\\" + vob_tag + "\\fpavmdkslreoila")) 
+    #def test_path_not_in_cc(self):
+    #    self.assertFalse(valid_path("C:\\Windows"))
+    def test_days_in_range(self):
+        self.assertTrue(valid_days(MIN_DAYS))
+        self.assertTrue(valid_days(MAX_DAYS))
+        self.assertTrue(valid_days((MIN_DAYS + MAX_DAYS) / 2))
+    def test_days_out_of_range(self):
+        self.assertFalse(valid_days(MIN_DAYS-1))
+        self.assertFalse(valid_days(MAX_DAYS+1))
+    #def test_vob_of_valid_path_in_snapshot(self):
+    #    self.assertEqual(get_vob_of_path("V:\\vob1\\scripts","vob1"))
+    #    self.assertEqual(get_vob_of_path("F:\\data\\views\\another_view_tag\\vob2\\somedir","vob2"))
+    #def test_vob_of_vob_path_in_snapshot(self):
+    #    self.assertEqual(get_vob_of_path("V:\\vob3","vob3"))
+    #def test_vob_of_valid_path_in_dyn_view(self):
+    #    self.assertEqual(get_vob_of_path("\\view\\some_view_tag\\vob4\\resources","vob4"))
+    #    self.assertEqual(get_vob_of_path("M:\\some_view_tag\\vob5\\dir1\\dir2","vob5"))
+        
 if __name__ == '__main__':
     unittest.main()
